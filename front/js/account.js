9a9f3b27b485bcc1613cc77ecc53e1ade09852e1
Henrik Karlsson <hk.henrik@gmail.com>
2016-07-17 00:00:53 +0200

    Inventory

--- front/js/account.js
+++ front/js/account.js
@@ -1,1 +1,29 @@
-console.log('acc.js')
+(function () {
+  const inventory = document.querySelector('.inventory')
+  const margin = 5
+  const size = 50
+
+  function createSlot (x, y) {
+    const slot = document.createElement('div')
+    slot.classList.add('inv-slot')
+    slot.style.left = margin + x * (margin + size)
+    slot.style.top = margin + y * (margin + size)
+    slot.id = `inv-${x}-${y}`
+
+    inventory.appendChild(slot)
+  }
+
+  function setItem (x, y, item) {
+    const slot = document.querySelector(`#inv-${x}-${y}`)
+    slot.innerHTML = `<img src="/png/${item}.png" draggable="true"></img>`
+  }
+
+  for (var y = 0; y < 10; y++) {
+    for (var x = 0; x < 10; x++) {
+      createSlot(x, y)
+    }
+  }
+
+
+  setItem(2, 2, 'itemsword')
+})()
