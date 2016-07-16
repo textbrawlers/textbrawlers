98c5db25ac624bd46c996d08be60c2afda15038d
Henrik Karlsson <hk.henrik@gmail.com>
2016-07-17 00:36:06 +0200

    Inventory drag-n-drop

--- front/js/account.js
+++ front/js/account.js
@@ -11,19 +11,48 @@
     slot.id = `inv-${x}-${y}`
 
     inventory.appendChild(slot)
+
+    return slot
   }
 
   function setItem (x, y, item) {
     const slot = document.querySelector(`#inv-${x}-${y}`)
-    slot.innerHTML = `<img src="/png/${item}.png" draggable="true"></img>`
+    slot.innerHTML = `<img src="/png/${item}.png"></img>`
   }
+
+  const slots = []
 
   for (var y = 0; y < 10; y++) {
     for (var x = 0; x < 10; x++) {
-      createSlot(x, y)
+      slots.push(createSlot(x, y))
     }
   }
 
+  slots.push(document.querySelector('.inv-head'))
+  slots.push(document.querySelector('.inv-body'))
+  slots.push(document.querySelector('.inv-legs'))
+  slots.push(document.querySelector('.inv-boots'))
+  slots.push(document.querySelector('.inv-lefthand'))
+  slots.push(document.querySelector('.inv-righthand'))
+
+  const drake = dragula(slots)
+
+  drake.on('drop', (elem, target, source) => {
+    window.target = target
+    window.source = source
+    console.log(target, source)
+
+    if (target.childNodes.length > 1) {
+      const prev = target.childNodes[0]
+      target.removeChild(prev)
+      source.appendChild(prev)
+    }
+  })
 
   setItem(2, 2, 'itemsword')
+  setItem(2, 1, 'itemshoes')
+  setItem(2, 3, 'itemchestpiece')
+  setItem(3, 3, 'itempants')
+  setItem(4, 3, 'itemshield')
+  setItem(5, 3, 'itemhelmet')
 })()
