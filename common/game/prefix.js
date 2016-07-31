10f08871b47dd87848d9af97dc7d5edeb0c7edcb
Henrik Karlsson <hk.henrik@gmail.com>
2016-07-31 21:55:53 +0200

    Fix item gen crash

--- common/game/prefix.js
+++ common/game/prefix.js
@@ -4,7 +4,20 @@ export default class {
   constructor(path, prefix) {
     this.path = path
 
+    prefix.characterStats = prefix.characterStats || []
+    prefix.attackStats = prefix.attackStats || []
+    prefix.empoweredStats = prefix.empoweredStats || []
+
+    this.characterStats = Object.entries(prefix.characterStats).map(([id, value]) => new Stat(id, value))
+    this.attackStats = Object.entries(prefix.attackStats).map(([id, value]) => new Stat(id, value))
+    
+    this.empoweredStats = prefix.empoweredStats.map(empowerConfig => {
+      return {
+        stats: Object.entries(empowerConfig.stats).map(([id, value]) => new Stat(id, value)),
+        category: empowerConfig.category
+      }
+    })
+
     this.name = prefix.name
-    this.stats = Object.entries(prefix.stats).map(([id, value]) => new Stat(id, value))
   }
 }
