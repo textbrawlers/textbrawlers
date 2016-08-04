ebdb05c9ec4f56f97e49afb3038aa1caab4d3165
Rasmus Steen <steen.rasmus.95@gmail.com>
2016-08-04 22:12:38 +0200

    servermower

--- server/api/game.js
+++ server/api/game.js
@@ -11,3 +11,29 @@ export async function requestItem(ctx){
 export async function requestInventory(ctx){
   ctx.body = ctx.player.inventory.serialize()
 }
+
+export async function moveItem(ctx){
+  let fromInv = getCorrectInventory(ctx, ctx.body.from.inventory)
+  let toInv = getCorrectInventory(ctx, ctx.body.to.inventory)
+  let fromPos = ctx.body.from.item
+  let toPos = ctx.body.to.item
+
+  let tempItem = fromInv[fromPos]
+  fromInv[fromPos] = toInv[toPos]
+  toInv[toPos] = tempItem
+
+  ctx.body = ctx.player.inventory.serialize()
+}
+
+function getCorrectInventory(ctx, inventory){
+  switch (inventory) {
+    case 'equipped':
+      return ctx.player.equipped
+    case 'inventory':
+      return ctx.player.inventory
+    case 'assembler':
+      return ctx.player.assembler
+    default:
+      return
+  }
+}
