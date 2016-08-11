658e29684a8b9dda35bb648f63d3b4e66db2ba21
MonsterPenguin <Alb.soderholm@gmail.com>
2016-08-11 23:17:45 +0200

    Vacker shiet

--- client/components/loginApp.js
+++ client/components/loginApp.js
@@ -9,14 +9,16 @@ export default class extends React.Component {
   render() {
     return (
       <div>
-        <div className="window signin-window">
-          <h2>Sign In</h2>
-        </div>
-        <div className="window signup-window">
-          <h2>Sign Up</h2>
-        </div>
-        <div className="window guest-window">
-          <h2>Play as Guest</h2>
+        <div className="container">
+          <div className="window signin-window">
+            <h2>Sign In</h2>
+          </div>
+          <div className="window signup-window">
+            <h2>Sign Up</h2>
+          </div>
+          <div className="window guest-window">
+            <h2>Play as Guest</h2>
+          </div>
         </div>
         {this.props.children}
       </div>
