beb280263f6d89c1f3766873c3374aca2caffb40
MonsterPenguin <Alb.soderholm@gmail.com>
2016-08-11 22:34:47 +0200

    Derp

--- client/components/loginApp.js
+++ client/components/loginApp.js
@@ -9,11 +9,9 @@ export default class extends React.Component {
   render() {
     return (
       <div>
-        <ul>
-          <li><Link to='/'> Home</Link></li>
-          <li><Link to='/login'> Login</Link></li>
-          <li><Link to='/register'> Register</Link></li>
-        </ul>
+        <div className="window login-window">
+          <h2>Equipped Items</h2>
+        </div>
         {this.props.children}
       </div>
     )
