0ae133fc11f4540c7594294ccbe7eeca43909baa
Rasmus Steen <steen.rasmus.95@gmail.com>
2016-08-19 21:29:20 +0200

    FIGHT

---
+++ common/api/specmath.js
@@ -0,0 +1,3 @@
+export function randomInt(value){
+  return Math.floor(Math.random() * value)
+}
---
+++ server/fight.js
@@ -0,0 +1,46 @@
+import SMath from "common/api/specmath"
+
+export default class Fight{
+  constructor(players){
+    this.playerStates = players.map(player => ({
+      currentHP: player.health,
+      maxHP: player.health,
+      player: player
+    }))
+
+    this.turn = SMath.randomInt(playerStates.length)
+    this.currentWeapon = 0
+  }
+
+  attack() {
+    this.attacker = this.playerStates[this.turn]
+    this.defender = this.playerStates[this.turn + 1] || this.playerStates[0]
+
+    this.weapons = this.attacker.player.weaponStats()
+
+    this.numAttacks = this.weapons[this.currentWeapon].getStat('attack-speed')
+
+    // Fakking mechanics
+
+    defender.hp -= this.weapons[this.currentWeapon].getStat('damage')
+
+    // Fakking mechanics
+
+    this.numAttacks -= 1
+
+    if (numAttacks <= 0) {
+      this.currentWeapon++
+      if (!this.weapons[this.currentWeapon]) {
+        this.turn ++
+        this.currentWeapon = 0
+        if (!this.playerStates[this.turn]) {
+          this.turn = 0
+        }
+      }
+    }
+    
+    if (defender.hp < 0) {
+      return true // Fight over
+    }
+  }
+}
