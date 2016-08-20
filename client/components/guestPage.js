7c478a16bb738b57cbcfdf3652945d810dfc2f34
MonsterPenguin <Alb.soderholm@gmail.com>
2016-08-20 18:40:37 +0200

    RETARDAT SHIT

--- client/components/guestPage.js
+++ client/components/guestPage.js
@@ -27,30 +27,8 @@ export default class RegisterPage extends React.Component {
             value={this.state.username}
             onChange={this.updateUsername.bind(this)} />
           <br />
-          <label htmlFor='password'>
-            Password:
-          </label>
-          <br />
-          <input
-            className='input'
-            type='password'
-            id='password'
-            value={this.state.password}
-            onChange={this.updatePassword.bind(this)} />
-          <br />
-          <label htmlFor='password-confirm'>
-            Confirm Password:
-          </label>
-          <br />
-          <input
-            className='input'
-            type='password'
-            id='password'
-            value={this.state.password}
-            onChange={this.updatePassword.bind(this)} />
-          <br />
           <div className='button-center'>
-            <input className='button' type='submit' value='Register' />
+            <input className='button' type='submit' value='Play as Guest' />
           </div>
         </form>
       </div>
--- client/components/homepageApp.js
+++ client/components/homepageApp.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router'
 import LoginPage from './loginPage.js'
 import RegisterPage from './registerPage.js'
+import GuestPage from './guestPage.js'
 import 'client/css/homepage.scss'
 
 export default class extends React.Component {
@@ -33,20 +34,25 @@ export default class extends React.Component {
         </div>
         <div className='page-homepage'>
           <div className='container-login'>
-            <div className='window login-window'>
-              <h2>Login</h2>
-              <div className='windowcontent'>
-                <LoginPage />
+            <div className="content-background">
+              <div className='window login-window'>
+                <h2>Login</h2>
+                <div className='windowcontent'>
+                  <LoginPage />
+                </div>
               </div>
-            </div>
-            <div className='window register-window'>
-              <h2>Register</h2>
-              <div className='windowcontent'>
-                <RegisterPage />
+              <div className='window register-window'>
+                <h2>Register</h2>
+                <div className='windowcontent'>
+                  <RegisterPage />
+                </div>
               </div>
-            </div>
-            <div className='window guest-window'>
-              <h2>Play as Guest</h2>
+              <div className='window guest-window'>
+                <h2>Play as Guest</h2>
+                <div className='windowcontent'>
+                  <GuestPage />
+                </div>
+              </div>
             </div>
           </div>
         </div>
