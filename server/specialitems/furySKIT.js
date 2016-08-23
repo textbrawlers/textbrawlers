e47199fc6f9bf6931720a60ccbcd1e7c1891c978
Rasmus Steen <steen.rasmus.95@gmail.com>
2016-08-23 19:23:13 +0200

    alla specialgrejor och set bunusar ska vara separata filer

---
+++ server/specialitems/furySKIT.js
@@ -0,0 +1,29 @@
+applyFury (weaponDamageMult) {
+  let attacker = this.getCurrentAttackerIndex()
+  if (this.playerStates[attacker].buffs.find(buff => buff.type === 'fury')) {
+    let buffIndex = this.playerStates[attacker].buffs.findIndex(buff => buff.type === 'fury')
+    const newBuff = {
+      type: 'fury',
+      damageMult: weaponDamageMult * this.playerStates[attacker].buffs[buffIndex].damageMult
+    }
+    this.playerStates[attacker].buffs[buffIndex] = newBuff
+  } else {
+    const newBuff = {
+      type: 'fury',
+      damageMult: weaponDamageMult
+    }
+    this.playerStates[attacker].buffs.push(newBuff)
+  }
+}
+
+resetFury () {
+  let attacker = this.getCurrentAttackerIndex()
+  if (this.playerStates[attacker].buffs.find(buff => buff.type === 'fury')) {
+    let buffIndex = this.playerStates[attacker].buffs.findIndex(buff => buff.type === 'fury')
+    const newBuff = {
+      type: 'fury',
+      damageMult: 1
+    }
+    this.playerStates[attacker].buffs[buffIndex] = newBuff
+  }
+}
