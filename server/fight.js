85fe3f611207bb5875253981e5252057dea44cad
Nicke535 <nicke@lofwenberg.com>
2016-08-23 15:53:13 +0200

    Fury fix
    Forgot the function to remove fury...great. It's there now, anyway.

--- server/fight.js
+++ server/fight.js
@@ -112,6 +112,10 @@ export default class Fight {
         this.defender.currentHP -= this.damage
       } else {
         this.miss = true
+        //Fury removing
+        if (this.playerStates[attacker].buffs.find(buff => buff.type === 'fury')) {
+          this.resetFury()
+        }
       }
     } else {
       this.hasWeapon = false
@@ -258,6 +262,18 @@ export default class Fight {
         damageMult: weaponDamageMult
       }
       this.playerStates[attacker].buffs.push(newBuff)
+    }
+  }
+  
+  resetFury () {
+    let attacker = this.getCurrentAttackerIndex()
+    if (this.playerStates[attacker].buffs.find(buff => buff.type === 'fury')) {
+      let buffIndex = this.playerStates[attacker].buffs.findIndex(buff => buff.type === 'fury')
+      const newBuff = {
+        type: 'fury',
+        damageMult: 1
+      }
+      this.playerStates[attacker].buffs[buffIndex] = newBuff
     }
   }
 
