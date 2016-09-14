f7e40723a2863dea29dbc0a0564e46fb9b93c897
Rasmus Steen <steen.rasmus.95@gmail.com>
2016-09-14 22:39:14 +0200

    pois does not apply automatically anymore

--- server/fight.js
+++ server/fight.js
@@ -218,29 +218,33 @@ export default class Fight {
     const oldBuffs = this.playerStates[defender].buffs
     const weapon = this.weapons[this.currentWeapon]
 
-    if (oldBuffs.find(buff => buff.type === 'poison')) {
-      this.log.push('ID: ' + this.attackId + '. Poison, dot found.')
-      const buffIndex = oldBuffs.findIndex(buff => buff.type === 'poison')
+    if (weapon.stats.getValue('poison-duration')){
+      if (oldBuffs.find(buff => buff.type === 'poison')) {
+        this.log.push('ID: ' + this.attackId + '. Poison, dot found.')
+        const buffIndex = oldBuffs.findIndex(buff => buff.type === 'poison')
 
-      if (oldBuffs[buffIndex].damage < weapon.stats.getValue('poison-damage')) {
-        this.log.push('ID: ' + this.attackId + '. Poison, new dot is stronger, applying.')
+        if (oldBuffs[buffIndex].damage < weapon.stats.getValue('poison-damage')) {
+          this.log.push('ID: ' + this.attackId + '. Poison, new dot is stronger, applying.')
+          const newBuff = {
+            type: 'poison',
+            duration: weapon.stats.getValue('poison-duration'),
+            damage: weapon.stats.getValue('poison-damage')
+          }
+          this.playerStates[defender].buffs[buffIndex] = newBuff
+        } else {
+          this.log.push('ID: ' + this.attackId + '. Poison, old dot is stonger.')
+        }
+      } else {
         const newBuff = {
           type: 'poison',
           duration: weapon.stats.getValue('poison-duration'),
           damage: weapon.stats.getValue('poison-damage')
         }
-        this.playerStates[defender].buffs[buffIndex] = newBuff
-      } else {
-        this.log.push('ID: ' + this.attackId + '. Poison, old dot is stonger.')
+        this.playerStates[defender].buffs.push(newBuff)
+        this.log.push('ID: ' + this.attackId + '. Poison, dot not found, applying.')
       }
-    } else {
-      const newBuff = {
-        type: 'poison',
-        duration: weapon.stats.getValue('poison-duration'),
-        damage: weapon.stats.getValue('poison-damage')
-      }
-      this.playerStates[defender].buffs.push(newBuff)
-      this.log.push('ID: ' + this.attackId + '. Poison, dot not found, applying.')
+    }else{
+      this.log.push('ID: ' + this.attackId + '. Poison, no poisonous weapon equipped.')
     }
   }
 
