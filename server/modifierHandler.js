75e14db02937526db225773892bcb627f16b47f0
Rasmus Steen <steen.rasmus.95@gmail.com>
2016-10-02 21:34:08 +0200

    sluta böga er

--- server/modifierHandler.js
+++ server/modifierHandler.js
@@ -2,7 +2,6 @@ import modifiers from './modifier/config.js'
 
 export default {
   apply (fightData) {
-<<<<<<< HEAD
     modifiers.forEach(modifier => {
       fightData = modifier.apply(fightData)
     })
@@ -13,26 +12,6 @@ export default {
 
   },
 
-=======
-    let resp = {}
-    modifiers.forEach(modifier => {
-      let mod = modifier.apply(fightData)
-
-      resp.damage = mod.damage
-      if (mod.blocked) {
-        resp.blocked = mod.blocked
-      }
-      if (mod.crits) {
-        resp.crits = mod.crits
-      }
-      if (mod.crits) {
-        resp.arcaneDamage = mod.arcaneDamage
-      }
-    })
-    return resp
-  },
-
->>>>>>> a3beb3a16769452a5ee6a62464999d92a5d63435
   tick (fightData) {
     let resp = []
     modifiers.forEach(modifier => {
