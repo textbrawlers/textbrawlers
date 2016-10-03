a1c302d1048965295ed8769a3758b73429448c21
Rasmus Steen <steen.rasmus.95@gmail.com>
2016-10-03 17:38:38 +0200

    example.js

---
+++ server/modifier/example.js
@@ -0,0 +1,28 @@
+// In config.js add an import row to the top of the file.
+// Example: import yourMod from 'yourMod.js'.
+// In this case yourMod is replaced with example.
+// Also add the name you set at import to the modifier array.
+// Example: arr = { otherMod, otherMod, yourMod }.
+// Where this code says "Modify fightData" you are to place all logic for that function.
+
+export default {
+  apply (fightData) {
+    // Modify fightData
+    return fightData
+  },
+
+  init (fightData) {
+    // Modify fightData
+    return fightData
+  },
+
+  tick (fightData) {
+    // Modify fightData
+    return fightData
+  },
+
+  end (fightData) {
+    // Modify fightData
+    return fightData
+  }
+}
