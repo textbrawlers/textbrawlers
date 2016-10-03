f33ead670d61a5c37e9308e4c6d07ccb606d906f
Henrik Karlsson <hk.henrik@gmail.com>
2016-10-03 21:46:29 +0200

    Fix bloodlust.js formatting

--- server/modifier/bloodlust.js
+++ server/modifier/bloodlust.js
@@ -9,14 +9,14 @@ export default {
   apply (fightData) {
     const defender = fightData.defenderIndex
     const weapon = fightData.weapons[fightData.currentWeapon]
-	let tempDamageCalc = 0
-	
+    let tempDamageCalc = 0
+
     const ps = fightData.playerStates[defender]
     ps.buffs.filter(buff => buff.type === 'bleed').forEach(buff => {
       tempDamageCalc++
     })
-	fightData.damage *= 1 + (Math.min((tempDamageCalc/10), weapon.stats.getValue('bloodlust')))
-	
+    fightData.damage *= 1 + (Math.min((tempDamageCalc / 10), weapon.stats.getValue('bloodlust')))
+
     return fightData
   }
 }
