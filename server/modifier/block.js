20a3d810fd75038c16e98e732017eb3d526b4f6d
Nicke535 <nicke@lofwenberg.com>
2016-10-06 12:56:35 +0200

    Block is now capped at 80% after armor pierce
    You can still build more block to counter armor pierce, but you can only ever have a 80% final block chance.

--- server/modifier/block.js
+++ server/modifier/block.js
@@ -1,7 +1,9 @@
 export default {
   apply (fightData) {
     fightData.textData.blocked = false
-    if (Math.random() < fightData.defender.player.stats.getValue('block-chance') - fightData.weapons[fightData.currentWeapon].stats.getValue('armor-pierce') && !fightData.miss) {
+    let effectiveBlock = Math.min(fightData.defender.player.stats.getValue('block-chance') - fightData.weapons[fightData.currentWeapon].stats.getValue('armor-pierce'), 0.8)
+    
+    if (Math.random() < effectiveBlock && !fightData.miss) {
       fightData.textData.blocked = true
       fightData.damage *= fightData.defender.player.stats.getValue('block-multiplier')
     }
