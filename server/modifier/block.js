1969ca5ce9a6432643d5e054784223bc461f8049
Nicke535 <nicke@lofwenberg.com>
2016-10-06 19:48:09 +0200

    No more trailing spaces!

--- server/modifier/block.js
+++ server/modifier/block.js
@@ -2,7 +2,7 @@ export default {
   apply (fightData) {
     fightData.textData.blocked = false
     let effectiveBlock = Math.min(fightData.defender.player.stats.getValue('block-chance') - fightData.weapons[fightData.currentWeapon].stats.getValue('armor-pierce'), 0.8)
-    
+
     if (Math.random() < effectiveBlock && !fightData.miss) {
       fightData.textData.blocked = true
       fightData.damage *= fightData.defender.player.stats.getValue('block-multiplier')
--- server/modifier/dodge.js
+++ server/modifier/dodge.js
@@ -2,20 +2,20 @@ export default {
   apply (fightData) {
     let dodgeMod = 0
     fightData.modifierStorage.didDodge = false
-    
+
     if (fightData.weapons[fightData.currentWeapon].stats.getValue('ranged') === true) {
       dodgeMod -= 0.1
     }
     if (fightData.weapons[fightData.currentWeapon].stats.getValue('slow') === true) {
       dodgeMod += 0.2
     }
-    
+
     if (Math.random() < fightData.defender.player.stats.getValue('dodge-chance') + dodgeMod) {
       fightData.miss = true
       fightData.modifierStorage.didDodge = true
       fightData.damage = 0
     }
-    
+
     return fightData
   }
 }
