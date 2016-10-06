ded930fe39473ddbc7621f28d313ea3b44a81f74
Nicke535 <nicke@lofwenberg.com>
2016-10-06 19:45:07 +0200

    Dodge PROPERLY fixed

--- server/modifier/dodge.js
+++ server/modifier/dodge.js
@@ -1,7 +1,8 @@
 export default {
   apply (fightData) {
     let dodgeMod = 0
-    let didDodge = [didDodge, false]
+    fightData.modifierStorage.didDodge = false
+    
     if (fightData.weapons[fightData.currentWeapon].stats.getValue('ranged') === true) {
       dodgeMod -= 0.1
     }
@@ -11,9 +12,8 @@ export default {
     
     if (Math.random() < fightData.defender.player.stats.getValue('dodge-chance') + dodgeMod) {
       fightData.miss = true
-      didDodge = [didDodge, true]
-      fightData.modifierStorage.push(didDodge)
-      fightData.damage *= 0
+      fightData.modifierStorage.didDodge = true
+      fightData.damage = 0
     }
     
     return fightData
