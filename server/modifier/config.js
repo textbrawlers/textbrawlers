ca3d4e24149935f543e0392101c896006763f8e3
Nicke535 <nicke@lofwenberg.com>
2016-10-06 12:45:46 +0200

    Updated the list to better reflect trigger order

--- server/modifier/config.js
+++ server/modifier/config.js
@@ -1,7 +1,7 @@
 // Include modifiers here.
 import dodge from './dodge.js'
-import crit from './crit.js'
 import block from './block.js'
+import crit from './crit.js'
 import arcane from './arcane.js'
 import bleed from './bleed.js'
 import burn from './burn.js'
@@ -11,19 +11,23 @@ import timestop from './timestop.js'
 import bloodlust from './bloodlust.js'
 import thorns from './thorns.js'
 
-// Add modifiers to array. Order matters.
+// Add modifiers to array. Order is as follows: Dodge, On Dodge Effects, Block, On Block Effects, Crit, On Crit Effects, On Hit Effects, Other
 const modifiers = [
   dodge,
-  crit,
+  
   block,
   thorns,
+  
+  crit,
+  
+  bloodlust,
   arcane,
   bleed,
   burn,
   poison,
   stun,
-  timestop,
-  bloodlust
+  
+  timestop
 ]
 
 export default modifiers
