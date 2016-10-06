4e2e00383d0dece7ef8e313cec2532f922a45a5a
Nicke535 <nicke@lofwenberg.com>
2016-10-06 12:51:44 +0200

    (Hopefully) added a way to detect if an attack was dodged

--- server/modifier/dodge.js
+++ server/modifier/dodge.js
@@ -1,6 +1,7 @@
 export default {
   apply (fightData) {
     let dodgeMod = 0
+    let didDodge = [didDodge, false]
     if (fightData.weapons[fightData.currentWeapon].stats.getValue('ranged') === true) {
       dodgeMod -= 0.1
     }
@@ -10,6 +11,8 @@ export default {
     
     if (Math.random() < fightData.defender.player.stats.getValue('dodge-chance') + dodgeMod) {
       fightData.miss = true
+      didDodge = [didDodge, true]
+      fightData.modifierStorage.Push(didDodge)
       fightData.damage *= 0
     }
     
