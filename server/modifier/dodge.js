c61d9b6bc72929c837257d7cd7a077ac0c905630
Nicke535 <nicke@lofwenberg.com>
2016-10-06 08:39:55 +0200

    Added Dodge
    Hopefully this should work flawlessly. But it wont.

---
+++ server/modifier/dodge.js
@@ -0,0 +1,18 @@
+export default {
+  apply (fightData) {
+    let dodgeMod = 0
+    if (fightData.weapons[fightData.currentWeapon].stats.getValue('ranged') === true) {
+      dodgeMod -= 0.1
+    }
+    if (fightData.weapons[fightData.currentWeapon].stats.getValue('slow') === true) {
+      dodgeMod += 0.2
+    }
+    
+    if (Math.random() < fightData.defender.player.stats.getValue('dodge-chance') + dodgeMod) {
+      fightData.miss = true
+      fightData.damage *= 0
+    }
+    
+    return fightData
+  }
+}
