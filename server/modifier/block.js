3cba21cd366caa1f87d46f0ac7d3b32e4ee03cb0
Nicke535 <nicke@lofwenberg.com>
2016-10-06 08:26:08 +0200

    Block no longer applies on a miss
    To clarify, it is no longer applied when a DIFFERENT MODIFIER causes an attack to miss. This should stop "thorns" or other on-block modifiers to work when an attack has completely missed.

--- server/modifier/block.js
+++ server/modifier/block.js
@@ -1,7 +1,7 @@
 export default {
   apply (fightData) {
     fightData.textData.blocked = false
-    if (Math.random() < fightData.defender.player.stats.getValue('block-chance') - fightData.weapons[fightData.currentWeapon].stats.getValue('armor-pierce')) {
+    if (Math.random() < fightData.defender.player.stats.getValue('block-chance') - fightData.weapons[fightData.currentWeapon].stats.getValue('armor-pierce') && !fightData.miss) {
       fightData.textData.blocked = true
       fightData.damage *= fightData.defender.player.stats.getValue('block-multiplier')
     }
