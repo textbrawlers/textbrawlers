e9c297b3c090709cf1ada8d1fe70314554125eb0
Henrik Karlsson <hk.henrik@gmail.com>
2016-10-07 22:22:25 +0200

    Happy Henrik är happy

--- server/realtime.js
+++ server/realtime.js
@@ -105,10 +105,15 @@ export default function realtime (wss) {
     if (player) {
       const realtimePlayer = new RealtimePlayer(player, ws, jsonUser)
       realtimePlayer.send('status.auth', true)
+
       players.filter(rtp => rtp.player.id.equals(player.id)).forEach(playerToDisconnect => {
         playerToDisconnect.ws.close()
-        players.splice(players.indexOf(playerToDisconnect), 1)
+        const index = players.indexOf(playerToDisconnect)
+        if (index === -1) {
+          players.splice(index, 1)
+        }
       })
+
       players.push(realtimePlayer)
       updatePlayers()
 
