091017b8f710bb64ded05764b788082c7c87713b
Nicke535 <nicke@lofwenberg.com>
2016-10-18 09:54:09 +0200

    Added Multistrike
    These things takes a lot of commits on the laptop...

--- server/modifier/config.js
+++ server/modifier/config.js
@@ -11,6 +11,7 @@ import timestop from './timestop.js'
 import bloodlust from './bloodlust.js'
 import thorns from './thorns.js'
 import timestopAccel from './timestopAccel.js'
+import multistrike from './multistrike.js'
 
 // Add modifiers to array. Order is as follows: Dodge, On Dodge Effects, Block, On Block Effects, Crit, On Crit Effects, On Hit Effects, Other
 const modifiers = [
@@ -27,6 +28,7 @@ const modifiers = [
   burn,
   poison,
   stun,
+  multistrike,
 
   timestop,
   timestopAccel
