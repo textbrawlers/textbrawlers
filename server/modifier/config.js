08ad117b68fb06a7b774be605e1d36fd0aa8ca7d
Nicke535 <nicke@lofwenberg.com>
2016-10-18 14:45:11 +0200

    Fix?

--- server/modifier/config.js
+++ server/modifier/config.js
@@ -10,7 +10,7 @@ import stun from './stun.js'
 import timestop from './timestop.js'
 import bloodlust from './bloodlust.js'
 import thorns from './thorns.js'
-import timestopAccel from './timestopAccel.js'
+import timestopaccel from './timestopaccel.js'
 import multistrike from './multistrike.js'
 
 // Add modifiers to array. Order is as follows: Dodge, On Dodge Effects, Block, On Block Effects, Crit, On Crit Effects, On Hit Effects, Other
@@ -31,7 +31,7 @@ const modifiers = [
   multistrike,
 
   timestop,
-  timestopAccel
+  timestopaccel
 ]
 
 export default modifiers
