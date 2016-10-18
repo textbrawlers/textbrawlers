701c0e3522765fb2adaabdb5829a3685e05bf1f7
Nicke535 <nicke@lofwenberg.com>
2016-10-18 10:04:25 +0200

    Multistrike code optimised

--- server/modifier/multistrike.js
+++ server/modifier/multistrike.js
@@ -1,22 +1,11 @@
 export default {
+  weaponChange (fightData) {
+    fightData.numAttacks *= fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike')
+    return fightData
+  },
+  
   apply (fightData) {
-    if (fightData.modifierStorage.multistrike.multistrikesLeft < 1) {
-      fightData.modifierStorage.multistrike = {
-        multistrikesLeft: 0
-      }
-    }
-
-    let multistrikes = fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike')
-
-    if (fightData.modifierStorage.multistrike.multistrikesLeft > 0) {
-      fightData.modifierStorage.multistrike.multistrikesLeft--
-    } else {
-      fightData.modifierStorage.multistrike.multestrikesLeft = multistrikes
-      fightData.numAttacks += multistrikes - 1
-    }
-
-    fightData.damage /= multistrikes
-
+    fightData.damage /= fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike')
     return fightData
   }
 }
