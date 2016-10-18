a041851108d441c35fa612a32c8f58ad9b659500
Nicke535 <nicke@lofwenberg.com>
2016-10-18 08:59:25 +0200

    Added "hasTimeStopped" to fightData.modifierStorage
    This enables other items to interact with timestop; i have plans for the Hourglass...

--- server/modifier/timestop.js
+++ server/modifier/timestop.js
@@ -1,7 +1,11 @@
 export default {
   end (fightData) {
+    fightData.modifierStorage.timestop = {
+      hasTimeStopped: false
+    }
     if (Math.random() < fightData.attacker.player.stats.getValue('timestop')) {
       fightData.turn = fightData.turn > 0 ? fightData.turn-- : fightData.playerStates.length - 1
+      fightData.modifierStorage.timestop.hasTimeStopped = true
     }
     return fightData
   }
