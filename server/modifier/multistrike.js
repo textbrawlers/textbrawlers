2a709d2d6e3b5eba5d7f0dac74c5f75146fcf943
Nicke535 <nicke@lofwenberg.com>
2016-10-20 09:00:22 +0200

    Multistrike no longer divides by zero
    ...yeah. That was a fail...

--- server/modifier/multistrike.js
+++ server/modifier/multistrike.js
@@ -1,11 +1,11 @@
 export default {
   weaponChange (fightData) {
-    fightData.numAttacks *= fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike')
+    fightData.numAttacks *= Math.max(fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike'), 1)
     return fightData
   },
 
   apply (fightData) {
-    fightData.damage /= fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike')
+    fightData.damage /= Math.max(fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike'), 1)
     return fightData
   }
 }
