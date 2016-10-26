427ea53d5d98ad3580eb7a4ef98c63df1ad67c5d
Nicke535 <nicke@lofwenberg.com>
2016-10-26 14:20:46 +0200

    Rampage?

--- server/modifier/config.js
+++ server/modifier/config.js
@@ -10,14 +10,15 @@ import stun from './stun.js'
 import timestop from './timestop.js'
 import bloodlust from './bloodlust.js'
 import thorns from './thorns.js'
-import maffect from './magicalaffect'
-import multistrike from './multistrike'
+import maffect from './magicalaffect.js'
+import multistrike from './multistrike.js'
+import rampage from './rampage.js'
 
 // Add modifiers to array. Order is as follows: Dodge, On Dodge Effects, Block, On Block Effects, Crit, On Crit Effects, On Hit Effects, Other
 const modifiers = [
   dodge,
 
-  multistrike,
+  rampage,
 
   block,
   thorns,
