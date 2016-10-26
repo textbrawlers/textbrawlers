12ed7c059002a05b1ef1722cd80c5b3fc605cfe6
Nicke535 <nicke@lofwenberg.com>
2016-10-26 10:58:57 +0200

    Multistrike reworked?

--- server/modifier/multistrike.js
+++ server/modifier/multistrike.js
@@ -1,11 +1,40 @@
 export default {
+  init (fightData) {    
+    let hitMod = 1 - (0.5 ^ fightData.modifierStorage.multistrike.multistrikeCounter )
+    
+    if (Math.random() < hitMod) {
+      fightData.modifierStorage.multistrike.remainingAttacks = fightData.numAttacks
+      fightData.numAttacks *= 0
+    }
+    return fightData
+  },
+  
   weaponChange (fightData) {
-    fightData.numAttacks *= Math.max(fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike'), 1)
+    fightData.modifierStorage.multistrike.multistrikeCounter = 0
     return fightData
   },
 
   apply (fightData) {
-    fightData.damage /= Math.max(fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike'), 1)
+    if (fightData.modifierStorage.multistrike.multistrikeCounter < fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike') - 1) {
+      fightData.modifierStorage.multistrike.multistrikeCounter++
+      if (fightData.numAttacks < 1) {
+        fightData.numAttacks = 2
+      } else {
+        fightData.numAttacks++
+      }
+      fightData.damage /= 2
+    } else {
+      fightData.modifierStorage.multistrike.multistrikeCounter = 0
+    }
+    
+    return fightData
+  },
+  
+  end (fightData) {
+    if (fightData.modifierStorage.multistrike.remainingAttacks) {
+      fightData.numAttacks += fightData.modifierStorage.multistrike.remainingAttacks
+      fightData.modifierStorage.multistrike.remainingAttacks = 0
+    }
     return fightData
   }
 }
