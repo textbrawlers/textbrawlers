abd04ad545819c801b96b71eb2e5642e4b897d46
Nicke535 <nicke@lofwenberg.com>
2016-10-26 18:43:10 +0200

    *Brainfart*

--- server/modifier/rampage.js
+++ server/modifier/rampage.js
@@ -1,6 +1,6 @@
 export default {
   apply (fightData) {
-    if (fightData.weapons[fightData.currentWeapon].stats.getValue('rampage-chance') < Math.random()) {
+    if (Math.random() < fightData.weapons[fightData.currentWeapon].stats.getValue('rampage-chance')) {
       if (fightData.numAttacks < 1) {
         fightData.numAttacks = 2
       } else {
