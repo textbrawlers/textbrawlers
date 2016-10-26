2a8356d55d854cef81241d0449b2aa806e6d0eb4
Nicke535 <nicke@lofwenberg.com>
2016-10-26 12:41:41 +0200

    Multistrike fixed?

--- server/modifier/multistrike.js
+++ server/modifier/multistrike.js
@@ -1,20 +1,26 @@
 export default {
   init (fightData) {    
     let hitMod = 1 - (0.5 ^ fightData.modifierStorage.multistrike.multistrikeCounter )
-    
+
     if (Math.random() < hitMod) {
       fightData.modifierStorage.multistrike.remainingAttacks = fightData.numAttacks
       fightData.numAttacks *= 0
     }
     return fightData
   },
-  
+
   weaponChange (fightData) {
-    fightData.modifierStorage.multistrike.multistrikeCounter = 0
+    fightData.modifierStorage.multistrike = {
+      multistrikeCounter: 0,
+      remainingAttacks: 0
+    }
     return fightData
   },
 
   apply (fightData) {
+    if (!fightData.modifierStorage.multistrike.multistrikeCounter) {
+      fightData.modifierStorage.multistrike.multistrikeCounter = 0
+    }
     if (fightData.modifierStorage.multistrike.multistrikeCounter < fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike') - 1) {
       fightData.modifierStorage.multistrike.multistrikeCounter++
       if (fightData.numAttacks < 1) {
@@ -26,10 +32,10 @@ export default {
     } else {
       fightData.modifierStorage.multistrike.multistrikeCounter = 0
     }
-    
+
     return fightData
   },
-  
+
   end (fightData) {
     if (fightData.modifierStorage.multistrike.remainingAttacks) {
       fightData.numAttacks += fightData.modifierStorage.multistrike.remainingAttacks
