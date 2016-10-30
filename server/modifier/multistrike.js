901e107303065f86cb6a930427b10f30a4aca389
Rasmus Steen <steen.rasmus.95@gmail.com>
2016-10-30 15:11:47 +0100

    possible stast of undefined fix

--- server/modifier/multistrike.js
+++ server/modifier/multistrike.js
@@ -1,10 +1,13 @@
 export default {
   apply (fightData) {
-    if (Math.random() < fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike-chance')) {
-      if (fightData.numAttacks < 1) {
-        fightData.numAttacks = 2
-      } else {
-        fightData.numAttacks++
+    const weapon = fightData.weapons[fightData.currentWeapon]
+    if (weapon.stats.getValue('multistrike-chance')) {
+      if (Math.random() < fightData.weapons[fightData.currentWeapon].stats.getValue('multistrike-chance')) {
+        if (fightData.numAttacks < 1) {
+          fightData.numAttacks = 2
+        } else {
+          fightData.numAttacks++
+        }
       }
     }
 
