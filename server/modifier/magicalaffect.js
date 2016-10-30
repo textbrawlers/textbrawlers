dfebd2725dc912db5e4c683e8d0ceaa2699bc1dc
Rasmus Steen <steen.rasmus.95@gmail.com>
2016-10-30 16:16:43 +0100

    Revert "Removed extra "if"-statement from Magical Affect"
    This reverts commit 5bd2a64c74da1635b5b9e6fc988179518e78d6fe.

--- server/modifier/magicalaffect.js
+++ server/modifier/magicalaffect.js
@@ -9,22 +9,23 @@ export default {
       if (oldBuffs.find(buff => buff.type === 'magical-affect')) {
         const buffIndex = oldBuffs.findIndex(buff => buff.type === 'magical-affect')
         const rand = Math.random()
-        const oldBuff = oldBuffs[buffIndex]
-
-        for (let i = 0; i < oldBuff.stacks; i++) {
-          chance /= 2
-        }
         if (rand < chance) {
-          const newBuff = {
-            type: 'magical-affect',
-            stacks: oldBuff.stacks + 1
-          }
-          fightData.playerStates[defender].buffs[buffIndex] = newBuff
-        } else {
+          const oldBuff = oldBuffs[buffIndex]
           for (let i = 0; i < oldBuff.stacks; i++) {
-            fightData.damage *= 1.5
+            chance /= 2
           }
-          fightData.playerStates[defender].buffs.splice(buffIndex, 1)
+          if (rand < chance) {
+            const newBuff = {
+              type: 'magical-affect',
+              stacks: oldBuff.stacks + 1
+            }
+            fightData.playerStates[defender].buffs[buffIndex] = newBuff
+          } else {
+            for (let i = 0; i < oldBuff.stacks; i++) {
+              fightData.damage *= 1.5
+            }
+            fightData.playerStates[defender].buffs.splice(buffIndex, 1)
+          }
         }
       } else if (Math.random() < chance) {
         const newBuff = {
