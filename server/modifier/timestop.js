b4f74d3790bf1f0a44c87423a71c36c6ef5e3ef1
Rasmus Steen <steen.rasmus.95@gmail.com>
2016-11-02 21:50:15 +0100

    testing timestop

--- server/modifier/timestop.js
+++ server/modifier/timestop.js
@@ -7,6 +7,7 @@ export default {
     }
     if (Math.random() < fightData.attacker.player.stats.getValue('timestop')) {
       fightData.turn = fightData.turn > 0 ? fightData.turn - 1 : fightData.playerStates.length - 1
+      fightData.currentWeapon = 0
       fightData.modifierStorage.timestop.hasTimeStopped = true
     }
     return fightData
--- server/modifierHandler.js
+++ server/modifierHandler.js
@@ -1,52 +1,48 @@
 import modifiers from './modifier/config.js'
+import _ from 'lodash'
 
-export default {
+const modifierHandler = {
   apply (fightData) {
     modifiers.filter(m => m.apply).forEach(modifier => {
+      const oldData = _.cloneDeep(fightData)
       fightData = modifier.apply(fightData)
-      if (!fightData) {
-        console.warn('FightData is undefined. Did you remember to return?')
-      }
+      modifierHandler.checkData(oldData, fightData, modifier)
     })
     return fightData
   },
 
   weaponChange (fightData) {
     modifiers.filter(m => m.weaponChange).forEach(modifier => {
+      const oldData = _.cloneDeep(fightData)
       fightData = modifier.weaponChange(fightData)
-      if (!fightData) {
-        console.warn('FightData is undefined. Did you remember to return?')
-      }
+      modifierHandler.checkData(oldData, fightData, modifier)
     })
     return fightData
   },
 
   init (fightData) {
     modifiers.filter(m => m.init).forEach(modifier => {
+      const oldData = _.cloneDeep(fightData)
       fightData = modifier.init(fightData)
-      if (!fightData) {
-        console.warn('FightData is undefined. Did you remember to return?')
-      }
+      modifierHandler.checkData(oldData, fightData, modifier)
     })
     return fightData
   },
 
   tick (fightData) {
     modifiers.filter(m => m.tick).forEach(modifier => {
+      const oldData = _.cloneDeep(fightData)
       fightData = modifier.tick(fightData)
-      if (!fightData) {
-        console.warn('FightData is undefined. Did you remember to return?')
-      }
+      modifierHandler.checkData(oldData, fightData, modifier)
     })
     return fightData
   },
 
   end (fightData) {
     modifiers.filter(m => m.end).forEach(modifier => {
+      const oldData = _.cloneDeep(fightData)
       fightData = modifier.end(fightData)
-      if (!fightData) {
-        console.warn('FightData is undefined. Did you remember to return?')
-      }
+      modifierHandler.checkData(oldData, fightData, modifier)
     })
     return fightData
   },
@@ -75,5 +71,25 @@ export default {
       }
     })
     return texts
+  },
+
+  checkData (oldData, newData, modifier) {
+    if (!newData) {
+      console.warn('@' + modifier.name + ': FightData is undefined. Did you remember to return?')
+    } else {
+      Object.entries(oldData).forEach(([oKey, oValue]) => Object.entries(newData).forEach(([nKey, nValue]) => {
+        if (oKey === nKey && oValue !== nValue) {
+          console.warn('@' + modifier.name + ': Variable ' + oKey.toString() + ' has changed from ' + oValue + ' to ' + nValue + '.')
+        }
+      }))
+      if (!newData.weapons[newData.currentWeapon]) {
+        console.warn('@' + modifier.name + ': Weapon at current index is undefined.')
+      }
+      if (newData.damage < 0 || newData.damage > 2000) {
+        console.warn('@' + modifier.name + ': Damage has an unexpected value: ' + newData.damage)
+      }
+    }
   }
 }
+
+export default modifierHandler
