8ae66d0c2d41c82dd31ff5200c212a46bb3ab633
Rasmus Steen <steen.rasmus.95@gmail.com>
2016-11-02 22:12:59 +0100

    time stop har chans på sista slaget per runda

--- server/modifier/timestop.js
+++ server/modifier/timestop.js
@@ -5,9 +5,11 @@ export default {
     fightData.modifierStorage.timestop = {
       hasTimeStopped: false
     }
-    if (Math.random() < fightData.attacker.player.stats.getValue('timestop')) {
-      fightData.turn = fightData.turn > 0 ? fightData.turn - 1 : fightData.playerStates.length - 1
-      fightData.modifierStorage.timestop.hasTimeStopped = true
+    if (fightData.numAttacks < 1 && fightData.weapons[fightData.currentWeapon + 1]) {
+      if (Math.random() < fightData.attacker.player.stats.getValue('timestop')) {
+        fightData.turn = fightData.turn > 0 ? fightData.turn - 1 : fightData.playerStates.length - 1
+        fightData.modifierStorage.timestop.hasTimeStopped = true
+      }
     }
     return fightData
   }
