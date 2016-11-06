91c880c982e3f54c470f8fbfdb3cbeaad84cfea5
Henrik Karlsson <hk.henrik@gmail.com>
2016-11-06 19:21:28 +0100

    More debug

--- server/modifier/timestop.js
+++ server/modifier/timestop.js
@@ -5,11 +5,13 @@ export default {
     if (Math.random() < fightData.attacker.player.stats.getValue('timestop') && !fightData.modifierStorage.timestoppedLastTurn) {
       fightData.turn = fightData.turn > 0 ? fightData.turn - 1 : fightData.playerStates.length - 1
       fightData.modifierStorage.timestop = true
+      console.log('timestop = true')
     }
     return fightData
   },
 
   newTurn (fightData) {
+    console.log('new turn')
     if (fightData.modifierStorage.timestop) {
       fightData.modifierStorage.timestop = false
       fightData.modifierStorage.timestoppedLastTurn = true
