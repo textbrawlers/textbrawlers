e27938879a818184c2b1789ee067de0471778f2e
Nicke535 <nicke@lofwenberg.com>
2016-11-10 08:52:07 +0100

    Ranged stat fully implemented (indirect bow nerf)

--- server/modifier/block.js
+++ server/modifier/block.js
@@ -3,7 +3,13 @@ export default {
 
   apply (fightData) {
     fightData.textData.blocked = false
-    let effectiveBlock = Math.min(fightData.defender.player.stats.getValue('block-chance') - fightData.weapons[fightData.currentWeapon].stats.getValue('armor-pierce'), 0.8)
+
+    let blockChanceMod = -fightData.weapons[fightData.currentWeapon].stats.getValue('armor-pierce')
+    if (fightData.weapons[fightData.currentWeapon].stats.getValue('ranged')) {
+      blockChanceMod += 0.2
+    }
+
+    let effectiveBlock = Math.min(fightData.defender.player.stats.getValue('block-chance') + blockChanceMod, 0.8)
 
     if (Math.random() < effectiveBlock) {
       fightData.textData.blocked = true
