80bee46dbac512206e4546f05fa922e552b9cf4a
Nicke535 <nicke@lofwenberg.com>
2016-11-11 22:23:08 +0100

    Reverts everywhere

--- server/modifier/arcane.js
+++ server/modifier/arcane.js
@@ -7,16 +7,6 @@ export default {
     const oldBuffs = fightData.playerStates[defender].buffs
     const weapon = fightData.weapons[fightData.currentWeapon]
 
-    if (weapon.stats.getValue('effect-archaic-amulet-of-arcane-augmentation') && oldBuffs.find(buff => buff.type === 'arcane')) {
-      const buffIndex = oldBuffs.findIndex(buff => buff.type === 'arcane')
-
-      if (oldBuffs[buffIndex].stacks < 2) {
-        chance *= 2
-      } else if (oldBuffs[buffIndex].stacks > 2) {
-        chance *= 0.5
-      }
-    }
-
     if (Math.random() < chance) {
       if (oldBuffs.find(buff => buff.type === 'arcane')) {
         const buffIndex = oldBuffs.findIndex(buff => buff.type === 'arcane')
