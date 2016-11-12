01b16f6f1123a56f03ec7547042e48d6e31d5838
Henrik Karlsson <hk.henrik@gmail.com>
2016-11-12 18:43:17 +0100

    Fix empty scss file crash

--- client/components/interface.js
+++ client/components/interface.js
@@ -5,7 +5,7 @@ export default class Game extends Component {
   render () {
     return (
       <div className='tb-interface'>
-        <div className='title-bar'><img src='/client/png/interface/title.png'></img></div>
+        <div className='title-bar'><img src='/client/png/interface/title.png' /></div>
         <div className='flex-container'>
           <div className='link-container'>
             <div className='link-cluster'>
