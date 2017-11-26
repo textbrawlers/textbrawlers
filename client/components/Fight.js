d9e29035ca14cc514a2666c0975b78cf6fa91729
MonsterPenguin <Alb.soderholm@gmail.com>
2017-11-26 23:45:02 +0100

    Fight Log

--- client/components/Fight.js
+++ client/components/Fight.js
@@ -12,7 +12,12 @@ import { Divider, Spacer } from './Divider.js'
 import Logo from './Logo.js'
 import InventorySlot from './InventorySlot.js'
 import { Item } from './Items.js'
-import { FightLogContainer, FightLogEvent, FightLogMarker } from './FightLog.js'
+import {
+  FightLogContainer,
+  FightLogEventContainer,
+  FightLogEvent,
+  FightLogMarker,
+} from './FightLog.js'
 
 const Fight = () => (
   <FullContainer>
@@ -22,10 +27,19 @@ const Fight = () => (
       <CenterContainer row>
         <Divider vertical fill />
         <FightLogContainer>
-          <FightLogEvent self>
-            <FightLogMarker />StenfiskeN got analklåda
-          </FightLogEvent>
+          <FightLogEventContainer>
+            <FightLogMarker />
+            <FightLogEvent self>StenfiskeN got analklåda</FightLogEvent>
+          </FightLogEventContainer>
+          <FightLogEventContainer enemy>
+            <FightLogEvent enemy>
+              Ineentho got analklåda and released a horde of camels to attack
+              his opponent in spite of the prophecy
+            </FightLogEvent>
+            <FightLogMarker enemy />
+          </FightLogEventContainer>
         </FightLogContainer>
+
         <Divider vertical fill />
       </CenterContainer>
     </CenterFlex>
--- client/components/FightLog.js
+++ client/components/FightLog.js
@@ -1,24 +1,57 @@
 import React from 'react'
 import styled from 'styled-components'
+import { lighten, darken } from 'polished'
 
 export const FightLogContainer = styled.div`
-  width: 600px;
-  background: red;
+  width: 500px;
   height: 400px;
   display: flex;
   flex-direction: column;
   flex-align: stretch;
 `
 
-export const FightLogEvent = styled.div`
+export const FightLogEventContainer = styled.div`
   display: flex;
   flex-direction: row;
   font-size: 16px;
-  margin: 10px 0;
+  border-bottom: 1px solid ${darken(0.15, 'rgb(0, 255, 0)')};
+  color: #1a1a1a;
+  &:not(:first-child) {
+    margin-top: 20px;
+  }
+
+  ${props =>
+    props.enemy &&
+    `
+    justify-content: flex-end;
+    border-bottom: 1px solid ${darken(0.05, 'rgb(255, 0, 0)')};
+`};
+`
+
+export const FightLogEvent = styled.div`
+  align-self: center;
+  font-size: 16px;
+  margin: 5px 0;
+
+  ${props =>
+    props.enemy &&
+    `
+    text-align: right;
+`};
 `
 
 export const FightLogMarker = styled.div`
+  align-self: stretch;
   margin: 0 10px 0 0;
-  width: 6px;
-  background: green;
+  height: initial;
+  width: 4px;
+  background: ${darken(0.15, 'rgb(0, 255, 0)')};
+  flex-shrink: 0;
+
+  ${props =>
+    props.enemy &&
+    `
+    margin: 0 0 0 10px;
+    background: ${darken(0.05, 'rgb(255, 0, 0)')};
+`};
 `
